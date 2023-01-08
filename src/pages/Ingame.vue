9a4786b0be60cde6c9fc73fa69adacac9194116b
rlapo213 <rlapo@naver.com>
2023-01-08 21:13:29 +0900

    Update Ingame.vue
    updated  opponent side

--- src/pages/Ingame.vue
+++ src/pages/Ingame.vue
@@ -9,12 +9,13 @@ function c() {
 }
 
 const UserSkills = User.value.skills;
+const OpponentSkills = Opponent.value.skills;
 console.log(User.value.cards);
 </script>
 
 <template>
   <div class="IngameRoot">
-    <div class="userside">
+    <div class="side">
       <div class="Skills">
         <div v-for="s in UserSkills" class="SkillInfo">
           <div>{{ s.name }}</div>
@@ -37,26 +38,53 @@ console.log(User.value.cards);
           </div>
         </div>
       </div>
-      <div class="User">
+      <div class="Player">
         <div>
           <div v-for="q in User.cards">
             <img class="ShapeImg" :src="NumberToShapeImg(q)" alt="" />
           </div>
         </div>
-        <div class="username"></div>
+        <div class="playername">Username</div>
         <div><img class="gene" src="../assets/Genes/Gene2.png" alt="" /></div>
         <div>hp</div>
         <div>shield</div>
       </div>
     </div>
     <div class="timer">timer</div>
-    <div class="opponentside">
-      <div>스킬들.</div>
-      <div>카드들.</div>
-      <div class="opponentname"></div>
-      <div><img src="" alt="" /></div>
-      <div>hp</div>
-      <div>shield</div>
+    <div class="side">
+      <div class="Skills rightside">
+        <div v-for="s in OpponentSkills" class="SkillInfo">
+          <div>{{ s.name }}</div>
+          <div class="Shapes">
+            <div v-for="q in s.resources">
+              <img class="ShapeImg" :src="NumberToShapeImg(q)" alt="" />
+            </div>
+          </div>
+          <div class="CombatElement" v-if="s.damage">
+            <img class="CombatImg" src="../assets/Combat/damage.png" alt="" />
+            <div>{{ s.damage }}</div>
+          </div>
+          <div class="CombatElement" v-if="s.shield">
+            <img class="CombatImg" src="../assets/Combat/shield.png" alt="" />
+            <div>{{ s.shield }}</div>
+          </div>
+          <div class="CombatElement" v-if="s.heal">
+            <img class="CombatImg" src="../assets/Combat/heal.png" alt="" />
+            <div>{{ s.heal }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="Player">
+        <div>
+          <div v-for="q in User.cards">
+            <img class="ShapeImg" :src="NumberToShapeImg(q)" alt="" />
+          </div>
+        </div>
+        <div class="playername">PotatoSheep</div>
+        <div><img class="gene" src="../assets/Genes/Gene2.png" alt="" /></div>
+        <div>hp</div>
+        <div>shield</div>
+      </div>
     </div>
   </div>
 </template>
@@ -69,7 +97,8 @@ console.log(User.value.cards);
 }
 .SkillInfo {
   display: flex;
-  margin: 1vw;
+  width: 30vw;
+  margin: 0.1vw;
   font-size: 2vw;
   border: 0.1vw solid white;
 }
@@ -87,7 +116,7 @@ console.log(User.value.cards);
   height: 100vh;
   font-size: 9vw;
 }
-.userside {
+.side {
   display: flex;
   flex-direction: column;
   width: 50vw;
@@ -99,7 +128,14 @@ console.log(User.value.cards);
   width: 50vw;
   height: 100%;
 }
-.User {
+.Skills {
+  display: flex;
+  flex-direction: column;
+}
+.rightside {
+  align-items: flex-end;
+}
+.Player {
   display: flex;
   flex-direction: column;
   align-items: center;
